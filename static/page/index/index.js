2f87ec1689f8513384bf1fbe027c35001097be7e
dailiqi <dailiqi@baidu.com>
2014-06-24 09:06:54 +0800

    add sm css

--- static/page/index/index.js
+++ static/page/index/index.js
@@ -1,9 +1,27 @@
 seajs.config({
 	base: "/static/",
 	alias : {
-		"jquery" : "/static/libs/jquery/dist/jquery.js",
+		"jquery" : "libs/jquery/dist/jquery.js"
 	}
 })
 // var sidebar = seajs.require('/static/ui/sidebar/sidebar.js')
-var sidebar = seajs.require('ui/sidebar/sidebar')
-sidebar.init();
+// var sidebar = seajs.require('ui/sidebar/sidebar')
+// sidebar.init();
+
+// seajs.use('ui/sidebar/main',function () {
+
+// 	alert(2);
+// 	// body...
+// });
+// seajs.use('ui/sidebar/main' , function() {
+// })
+// require('ui/sidebar/main');
+define('page/index/index' ,function(require,exports,module) {
+	var Toolbar = require('ui/toolbar/toolbar').Toolbar;
+	var toolbar = new Toolbar('#container');
+	toolbar.render();
+	// var toolbar = new Toolbar();
+	// var sidebar = require('ui/sidebar/sidebar')
+	// console.log(sidebar);
+})
+seajs.use('page/index/index');
--- static/ui/sidebar/sidebar.js
+++ static/ui/sidebar/sidebar.js
@@ -1,6 +1,27 @@
+define('ui/sidebar/sidebar',function(require, exports, module) {
+	alert(1);
+	var data = require('./data');
+  var $ = require('jquery');
+  var Sidebar = function (container) {
+    this._container = $(container);
+  }
+  Sidebar.prototype.init = function() {
+    this._container.append('<div>sidebar init</div>');
+  };
+  // 模块代码
+  // var a = require('./a'); 
+  //exports是module.exports的一个引用，只能通过赋值来添加
+  // exports.foo = 'bar';
+  exports.Sidebar = Sidebar;
+  //module.exports支持覆盖赋值
+  // module.exports = {
+  //   foo: 'bar'
+  // }
+});
+
 // // var Sidebar = {};
 // // Sidebar.prototype.init = function() {
-// // 	alert('Sidebar init');
+// //   alert('Sidebar init');
 // // };
 // // Module.exports = Sidebar;
 
@@ -13,10 +34,10 @@
 //   // 通过 exports 对外提供接口
 //   // exports.doSomething = ...
 //   exports.init = function() {
-//   	alert('init');
+//    alert('init');
 //   }
 //   var Sidebar = function (container) {
-//   	this._container = $(container);
+//    this._container = $(container);
 //   }
 //   module.exports = Sidebar;
 
@@ -24,15 +45,3 @@
 //   // module.exports =Sidebar;
 
 // });
-define('ui/sidebar/sidebar',function(require, exports, module) {
-	alert(1);
-	var data = require('./data');
-  // 模块代码
-  // var a = require('./a'); 
-  //exports是module.exports的一个引用，只能通过赋值来添加
-  exports.foo = 'bar';
-  //module.exports支持覆盖赋值
-  module.exports = {
-    foo: 'bar'
-  }
-});
